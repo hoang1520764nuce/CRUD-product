235c38ef07001cb909cf1bdd2995d5d1caf1d732
hoang1520764nuce <hoang1520764@nuce.edu.vn>
2022-12-20 01:41:09 +0700

    change

---
+++ database/init.sql
@@ -0,0 +1,13 @@
+create table employee (
+    id serial primary key,
+    name varchar(255) not null,
+    phone varchar(255) not null,
+    email varchar(255) not null,
+    address varchar(255) not null,
+    salary integer not null,
+    
+);
+
+
+
+
